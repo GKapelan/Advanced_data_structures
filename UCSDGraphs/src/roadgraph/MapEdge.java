53b9799947f3472a4129a24cb6b63d3ede4638a3
Shishkin Semen <semgap@gmail.com>
2020-07-19 00:32:06 +0300

    Week four task: Djikstra + A-Star Also Saint-Petersburg map

--- UCSDGraphs/src/roadgraph/MapEdge.java
+++ UCSDGraphs/src/roadgraph/MapEdge.java
@@ -1,32 +1,146 @@
 package roadgraph;
 
+import geography.GeographicPoint;
+
 public class MapEdge {
 	
+	/** The two end points of the edge */
 	private MapNode start;
 	private MapNode goal;
+	/** The name of the road */
+	private String roadName;
+	/** The type of the road */
+	private String roadType;
+	/** The length of the road segment, in km */
+	private double length;
+	static final double DEFAULT_LENGTH = 0.01;
 	
-	public MapEdge(MapNode s, MapNode g) {
-		// TODO Auto-generated constructor stub
-		
-		start = s;
-		goal  = g;
+	/** Create a new MapEdge object
+	 * 
+	 * @param roadName
+	 * @param n1  The point at one end of the segment
+	 * @param n2  The point at the other end of the segment
+	 * 
+	 */
+	MapEdge(String roadName, MapNode n1, MapNode n2) 
+	{
+		this(roadName, "", n1, n2, DEFAULT_LENGTH);
 	}
 	
+	/** 
+	 * Create a new MapEdge object
+	 * @param roadName  The name of the road
+	 * @param roadType  The type of the road
+	 * @param n1 The point at one end of the segment
+	 * @param n2 The point at the other end of the segment
+	 */
+	MapEdge(String rName, String rType, MapNode n1, MapNode n2) 
+	{
+		this(rName, rType, n1, n2, DEFAULT_LENGTH);
+	}
+	
+	/** 
+	 * Create a new MapEdge object
+	 * @param roadName  The name of the road
+	 * @param roadType  The type of the road
+	 * @param n1 The point at one end of the segment
+	 * @param n2 The point at the other end of the segment
+	 * @param length The length of the road segment
+	 */	
+	MapEdge(String rName, String rType,	MapNode n1, MapNode n2, double l) 
+	{
+		roadName = rName;
+		start = n1;
+		goal  = n2;
+		roadType = rType;
+		length   = l;
+	}
+	
+	/**
+	 * Return the location of the start point
+	 * @return The location of the start point as a GeographicPoint
+	 */
 	public MapNode getStart() {
 		return start;
 	}
 
+	/**
+	 * return the MapNode for the goal point
+	 * @return the MapNode for the goal point
+	 */
 	public MapNode getGoal() {
 		return goal;
 	}
 	
-	public MapNode getOtherNode(MapNode node)
+	/**
+	 * Return the location of the start point
+	 * @return The location of the start point as a GeographicPoint
+	 */
+	GeographicPoint getStartPoint()
 	{
-		MapNode result = null;
+		return start.getLocation();
+	}
+	
+	/**
+	 * Return the location of the goal point
+	 * @return The location of the goal point as a GeographicPoint
+	 */
+	GeographicPoint getEndPoint()
+	{
+		return goal.getLocation();
+	}
+	
+	/**
+	 * Get the road's name
+	 * @return the name of the road that this edge is on
+	 */
+	public String getRoadName() {
+		return roadName;
+	}
+
+	/**
+	 * return the MapNode for the end point
+	 * @return the MapNode for the end point
+	 */
+	public String getRoadType() {
+		return roadType;
+	}
+	
+	/**
+	 * Return the length of this road segment
+	 * @return the length of the road segment
+	 */
+	public double getLength() {
+		return length;
+	}	
+		
+	/**
+	 * Given one of the nodes involved in this edge, get the other one
+	 * @param node The node on one side of this edge
+	 * @return the other node involved in this edge
+	 */
+	MapNode getOtherNode(MapNode node)
+	{
 		if (node.equals(start)) 
-			result = goal;
+			return goal;
 		else if (node.equals(goal))
-			result = start;
-		return result;
+			return start;
+		throw new IllegalArgumentException("Looking for " +
+			"a point that is not in the edge");
+	}
+	
+	/**
+	 * Return a String representation for this edge.
+	 */
+	@Override
+	public String toString()
+	{
+		String toReturn = "[EDGE between ";
+		toReturn += "\n\t" + start.getLocation();
+		toReturn += "\n\t" + goal.getLocation();
+		toReturn += "\nRoad name: " + roadName + " Road type: " + roadType +
+				" Segment length: " + String.format("%.3g", length) + "km";
+		
+		return toReturn;
 	}
 }
--- UCSDGraphs/src/roadgraph/MapNode.java
+++ UCSDGraphs/src/roadgraph/MapNode.java
@@ -5,30 +5,57 @@ import java.util.Set;
 
 import geography.GeographicPoint;
 
-public class MapNode{
+
+public class MapNode implements Comparable {
 	
+	/** The list of edges out of this node */
 	private HashSet<MapEdge> edges;
-	
+	/** The latitude and longitude of this node */
 	private GeographicPoint location;
 	
+	/** Distance between nodes */
+	private double distance;
+	private double actualDistance;
+	
+	/** 
+	 * Create a new MapNode at a given Geographic location
+	 * @param loc the location of this node
+	 */
 	MapNode(GeographicPoint loc) {
 		location = loc;
-		edges = new HashSet<MapEdge>();
+		edges    = new HashSet<MapEdge>();
+		distance = 0.0;
 	}
 	
-	public HashSet<MapEdge> getEdges() {
+	/**
+	 * return the edges out of this node
+	 * @return a set containing all the edges out of this node.
+	 */
+	public Set<MapEdge> getEdges() {
 		return edges;
 	}
 	
+	/**
+	 * Get the geographic location that this node represents
+	 * @return the geographic location of this node
+	 */
 	public GeographicPoint getLocation() {
 		return location;
 	}
 	
+	/**
+	 * Add an edge that is outgoing from this node in the graph
+	 * @param edge The edge to be added
+	 */
 	public void addEdge(MapEdge edge)
 	{
 		edges.add(edge);
 	}
 	
+	/**  
+	 * Return the neighbors of this MapNode 
+	 * @return a set containing all the neighbors of this node
+	 */
 	public Set<MapNode> getNeighbors()
 	{
 		Set<MapNode> neighbors = new HashSet<MapNode>();
@@ -36,5 +63,99 @@ public class MapNode{
 			neighbors.add(edge.getOtherNode(this));
 		}
 		return neighbors;
+	}
+
+	/**
+	 * Add distance
+	 * @param d
+	 */
+	public void setDistance(double d) {
+		distance = d;
+	}
+
+	/**
+	 * Return distance
+	 * @return distance
+	 */
+	public double getDistance() {
+		return distance;
+	}
+	
+	/**
+	 * Add actualDistance
+	 * @param actualDistance
+	 */
+	public void setActualDistance(double actualDistance) {
+		this.actualDistance = actualDistance;
+	}
+	
+	/**
+	 * Return actualDistance
+	 * @return actualDistance
+	 */
+	public double getActualDistance() {
+		return actualDistance;
+	}
+	
+	/**
+	 * Implement Comparable
+	 */
+	public int compareTo(Object o) {
+		// convert to map node, may throw exception
+		MapNode m = (MapNode)o; 
+		return ((Double)this.getDistance()).compareTo((Double) m.getDistance());
+	}
+	
+	/** Returns whether two nodes are equal.
+	 * Nodes are considered equal if their locations are the same, 
+	 * even if their street list is different.
+	 * @param o the node to compare to
+	 * @return true if these nodes are at the same location, false otherwise
+	 */
+	@Override
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof MapNode) || (o == null)) {
+			return false;
+		}
+		MapNode node = (MapNode)o;
+		return node.location.equals(this.location);
+	}
+	
+	/** Because we compare nodes using their location, we also 
+	 * may use their location for HashCode.
+	 * @return The HashCode for this node, which is the HashCode for the 
+	 * underlying point
+	 */
+	@Override
+	public int hashCode()
+	{
+		return location.hashCode();
+	}
+	
+	/** ToString to print out a MapNode object
+	 *  @return the string representation of a MapNode
+	 */
+	@Override
+	public String toString()
+	{
+		String toReturn = "[NODE at location (" + location + ")";
+		toReturn += " intersects streets: ";
+		for (MapEdge e: edges) {
+			toReturn += e.getRoadName() + ", ";
+		}
+		toReturn += "]";
+		return toReturn;
+	}
+
+	// For debugging, output roadNames as a String.
+	public String roadNamesAsString()
+	{
+		String toReturn = "(";
+		for (MapEdge e: edges) {
+			toReturn += e.getRoadName() + ", ";
+		}
+		toReturn += ")";
+		return toReturn;
 	}
 }
